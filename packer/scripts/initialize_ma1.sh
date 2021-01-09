2f0a7d1c9df233474c3edc32fa7b69068a5c7a9f
wzh <wangzheng422@gmail.com>
2021-01-09 19:48:47 +0800

    fix dhcp issue

--- packer/scripts/initialize_ma1.sh
+++ packer/scripts/initialize_ma1.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-sudo dhclient ma1
+# sudo dhclient ma1
 
-sleep 5
+# sleep 5
 
-MGMT_IP=`ip addr show dev ma1 | grep inet | awk -F\  '{print $2}'`
+# MGMT_IP=`ip addr show dev ma1 | grep inet | awk -F\  '{print $2}'`
 
 FastCli -p 15 -c "configure
 interface Management1
-  ip address $MGMT_IP
+  ip address dhcp
 exit
 wr mem"
 
-DHCLIENT_PID=`ps fauxwww | grep -v grep | grep dhclient | awk -F\  '{print $2}'`
+# DHCLIENT_PID=`ps fauxwww | grep -v grep | grep dhclient | awk -F\  '{print $2}'`
 
-sudo kill -9 $DHCLIENT_PID
+# sudo kill -9 $DHCLIENT_PID
